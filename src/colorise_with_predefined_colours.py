6afdbfb36229577b3c08da92a8427f581916b2dd
giovannicampa <giocampa93@gmail.com>
2022-03-27 20:09:48 +0200

    Added argument parser

--- src/colorise_with_predefined_colours.py
+++ src/colorise_with_predefined_colours.py
@@ -2,6 +2,7 @@ from colour_transfer import ColorTransfer
 import numpy as np
 import json
 import matplotlib.pyplot as plt
+import argparse
 
 DISTANCE_COLORS_MAX = 1E9
 NR_COMBINATIONS = 348
@@ -39,38 +40,56 @@ def sort_colors_local_min(colors_original: np.ndarray, colors_new, distance_func
                 color_index_to_delete = i
 
         colors_new_sorted.append(color_match)
-        colors.pop(color_index_to_delete)
+        colors_new.pop(color_index_to_delete)
 
     return colors_new_sorted
 
 
+def colorize_image_all_combinations():
 
-# Function that calculates the distance between two colors
-distance_function = np.linalg.norm
+    nr_color_combinations = args.nr_colors
+    input_path = args.input_path
 
-coloriser = ColorTransfer(nr_clusters=4)
+    # Function that calculates the distance between two colors
+    distance_function = np.linalg.norm
 
-image, image_norm = coloriser.read_image(path_image = "/home/giovanni/Downloads/giovanno.jpeg")
+    coloriser = ColorTransfer(nr_clusters=nr_color_combinations)
 
-with open("/home/giovanni/PyProjects/dictionary-of-colour-combinations/colors.json") as jsonFile:
-    jsonObject = json.load(jsonFile)
-    jsonFile.close()
+    image, image_norm = coloriser.read_image(path_image = input_path)
+
+    with open("/home/giovanni/PyProjects/dictionary-of-colour-combinations/colors.json") as jsonFile:
+        jsonObject = json.load(jsonFile)
+        jsonFile.close()
 
 
-labels, colors_original, color_frequency_original = coloriser.clusterise_image(image/255)
+    labels, colors_original, color_frequency_original = coloriser.clusterise_image(image/255)
 
-for combination in range(NR_COMBINATIONS):
-    colors = []
-    for color in jsonObject:
-        if combination in color["combinations"]:
-            colors.append(np.array(color["rgb"]))
-    
-    if len(colors) < 4: continue
+    for combination in range(NR_COMBINATIONS):
+        colors = []
+        for color in jsonObject:
+            if combination in color["combinations"]:
+                colors.append(np.array(color["rgb"]))
+        
+        if len(colors) < nr_color_combinations: continue
 
-    # Sort colors by method
-    colors_new = sort_colors_local_min(colors_original, colors_new, distance_function)
+        # Sort colors by method
+        colors_new = sort_colors_local_min(colors_original, colors, distance_function)
 
-    image_quantized = coloriser.quantise_image(image_norm, labels, colors_new)
+        image_quantized = coloriser.quantise_image(image_norm, labels, colors_new)
 
-    plt.imshow(image_quantized)
-    plt.show()
+        plt.imshow(image_quantized)
+        plt.show()
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser("Parser for ")
+    parser.add_argument("-n", "--nr_colors", help="How many colors have to be clustered.", type=int, default=4)
+    parser.add_argument("-i", "--input_path", help="Path to input image", type=str, default="/home/giovanni/Downloads/giovanno.jpeg")
+    parser.add_argument("-o", "--output_path", help="Path to save path", type=str)
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+
+    args = parse_arguments()
+    colorize_image_all_combinations()
