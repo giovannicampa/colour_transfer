cc0291bf2a7a191ac66081cb8493e43f4f290374
giovannicampa <giocampa93@gmail.com>
2022-03-31 10:39:55 +0200

    Color sorterwith hungarian algorithm

--- src/colorise_with_predefined_colours.py
+++ src/colorise_with_predefined_colours.py
@@ -4,11 +4,12 @@ import json
 import matplotlib.pyplot as plt
 import argparse
 import colorsys
+from scipy.optimize import linear_sum_assignment
 
 DISTANCE_COLORS_MAX = 1E9
 NR_COMBINATIONS = 348
 
-def sort_colors_local_min(colors_original: np.ndarray, colors_new, distance_function) -> list:
+def sort_colors_local_min(colors_original: list, colors_new: list, distance_function) -> list:
     """
     Sort colors by method defined in distance_function.
     
@@ -46,6 +47,44 @@ def sort_colors_local_min(colors_original: np.ndarray, colors_new, distance_func
     return colors_new_sorted
 
 
+def sort_colors_global_min(colors_original: list, colors_new: list, distance_function) -> list:
+    """
+    Sort colors by method defined in distance_function.
+    
+    Finds matching colors to the new ones by using the hungarian algorithm.
+    First a distance matrix between color pairs is built up. The algorithm then finds the matches that
+    generate the minimum summed distance cost.
+
+    Both the original and the new colors are defined as np.ndarrays of size [3 x n].
+    These contain, for n colors, their encoding as three rgb values.
+
+    Args:
+        colors_original: colors of the original image
+        colors_new: new colors that have to be applied
+        distance_function: function that calculates the distance between two colors values
+    
+    Returns:
+        colors_new_sorted: the new color values
+    """
+
+    shape = len(colors_original)
+    distance_between_colors = np.zeros(shape = (shape, shape))
+
+    colors_new_sorted = []
+    for i, color_original in enumerate(colors_original):
+        
+        for j, color_new in enumerate(colors_new):
+
+            distance_between_colors[i,j] = distance_function(color_new[0:2] - color_original[0:2])
+
+    _, rows = linear_sum_assignment(distance_between_colors)
+
+    colors_new = np.array(colors_new)
+    colors_new_sorted = colors_new[rows]
+
+    return colors_new_sorted
+
+
 def colorize_image_all_combinations():
 
     nr_color_combinations = args.nr_colors
@@ -53,6 +92,12 @@ def colorize_image_all_combinations():
 
     # Function that calculates the distance between two colors
     distance_function = np.linalg.norm
+
+    # Function that sorts the colors
+    if args.method == "local":
+        sort_colors_function = sort_colors_local_min
+    elif args.method == "global":
+        sort_colors_function = sort_colors_global_min
 
     coloriser = ColorTransfer(nr_clusters=nr_color_combinations)
 
@@ -78,7 +123,7 @@ def colorize_image_all_combinations():
             colors_original = [np.array(colorsys.rgb_to_hsv(*color)) for color in colors_original]
 
         # Sort colors by method
-        colors_new = sort_colors_local_min(colors_original, colors, distance_function)
+        colors_new = sort_colors_function(colors_original, colors, distance_function)
 
         if args.color_method == "hsv":
             colors_new = [np.array(colorsys.hsv_to_rgb(*color)) for color in colors_new]
